b0ec1f7b70545e7ae38f36ac7424c913b51f6f9a
jeffry1829 <jeffry1829@gmail.com>
2022-06-21 01:42:03 +0800

    fix trace

--- tracetest/tracetest.cpp
+++ tracetest/tracetest.cpp
@@ -47,52 +47,82 @@
 using namespace cytnx;
 using namespace std;
 //#define int long long
-#define rep(i,a,n) for(int i=a;i<n;i++)
-#define per(i,a,n) for(int i=n-1;i>=a;i--)
+#define rep(i, a, n) for (int i = a; i < n; i++)
+#define per(i, a, n) for (int i = n - 1; i >= a; i--)
 #define pb push_back
 //#define mp make_pair
-#define all(x) (x).begin(),(x).end()
+#define all(x) (x).begin(), (x).end()
 #define fi first
 #define se second
 #define SZ(x) ((int)(x).size())
-#define min(a,b) (((a)<(b))?(a):(b))
-#define max(a,b) (((a)>(b))?(a):(b))
-#define abs(x) (((x)<0)?(-(x)):(x))
+#define min(a, b) (((a) < (b)) ? (a) : (b))
+#define max(a, b) (((a) > (b)) ? (a) : (b))
+#define abs(x) (((x) < 0) ? (-(x)) : (x))
 typedef vector<int> VI;
 typedef long long ll;
-typedef pair<int,int> PII;
+typedef pair<int, int> PII;
 typedef double db;
 mt19937 mrand(random_device{}());
-const ll mod=1000000007;
-int rnd(int x){return mrand()%x;}
-ll powmod(ll a,ll b){ll res=1;a%=mod;assert(b>=0);for(;b;b>>=1){if(b&1)res=res*a%mod;a=a*a%mod;}return res;}
-ll gcd(ll a, ll b){return b?gcd(b,a%b):a;}
-inline int pmod(int x, int d){int m = x%d;return m+((m>>31)&d);}
+const ll mod = 1000000007;
+int rnd(int x) { return mrand() % x; }
+ll powmod(ll a, ll b) {
+  ll res = 1;
+  a %= mod;
+  assert(b >= 0);
+  for (; b; b >>= 1) {
+    if (b & 1) res = res * a % mod;
+    a = a * a % mod;
+  }
+  return res;
+}
+ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a; }
+inline int pmod(int x, int d) {
+  int m = x % d;
+  return m + ((m >> 31) & d);
+}
 #define y1 ojsapogjahg
 #define prev ojaposjdas
 #define rank oiajgpowsdjg
 #define left aijhgpiaejhgp
 //#define end aononcncnccc
-//head
+// head
 #include <omp.h>
 
-int main(void){
-	const int N=2,M=1024;
-	Tensor T = random::uniform(N*M*M*N,10,10000);
-	T.reshape_(N,M,M,N);
-	UniTensor UT = UniTensor(T,2);
-	UT.print_diagram();
+int main(void) {
+  using std::chrono::duration;
+  using std::chrono::duration_cast;
+  using std::chrono::high_resolution_clock;
+  using std::chrono::milliseconds;
 
-    // int Nomp;
-    // #pragma omp parallel
-    // {
-        // if(omp_get_thread_num()==0) Nomp = omp_get_num_threads();
-    // }
-    // cout << Nomp <<endl;
+  auto t1 = high_resolution_clock::now();
+  
+  const int N = 2, M = 1024;
+  Tensor T = random::uniform(N * M * M * N, 10, 10000);
+  T.reshape_(N, M, M, N);
+  UniTensor UT = UniTensor(T, false, 2);
+  UT.print_diagram();
 
-	// cout<< omp_get_max_threads() <<endl;
+  // int Nomp;
+  // #pragma omp parallel
+  // {
+  // if(omp_get_thread_num()==0) Nomp = omp_get_num_threads();
+  // }
+  // cout << Nomp <<endl;
 
-	UniTensor UT2 = UT.Trace(0,3);
-	UT2.print_diagram();
-	return 0;
+  // cout<< omp_get_max_threads() <<endl;
+
+  UniTensor UT2 = UT.Trace(0, 3);
+  UT2.print_diagram();
+  
+  auto t2 = high_resolution_clock::now();
+
+  /* Getting number of milliseconds as an integer. */
+  auto ms_int = duration_cast<milliseconds>(t2 - t1);
+
+  /* Getting number of milliseconds as a double. */
+  duration<double, std::milli> ms_double = t2 - t1;
+
+  std::cout << ms_int.count() << "ms\n";
+  std::cout << ms_double.count() << "ms\n";
+  return 0;
 }
